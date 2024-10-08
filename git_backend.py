687faf62eaa924a07a34514cfea9d515238cc0bb
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-10-08 04:08:21 +0300

    Update git_backend.py

--- git_backend.py
+++ git_backend.py
@@ -147,76 +147,76 @@ else:
     page = st.sidebar.radio('Select page', ['STG-2024', 'View Logs'])
     
     if page == 'STG-2024':
-        def main():
+        
+        st.markdown("""
+        <style>
+            .stProgress > div > div > div {
+                background-color: #FFD700;
+                border-radius: 50%;
+            }
+        </style>
+        """, unsafe_allow_html=True)
+        
+        with st.spinner("Data loaded successfully!"):
+            import time
+            time.sleep(1)
+        
+        col1, col2 = st.columns([2, 0.75])
+        with col1:
             st.markdown("""
-            <style>
-                .stProgress > div > div > div {
-                    background-color: #FFD700;
-                    border-radius: 50%;
-                }
-            </style>
+                <h2 style='text-align: center; font-size: 40px; color: red;'>
+                    Find your parts
+                </h2>
             """, unsafe_allow_html=True)
-            
-            with st.spinner("Data loaded successfully!"):
-                import time
-                time.sleep(1)
-            
-            col1, col2 = st.columns([2, 0.75])
-            with col1:
-                st.markdown("""
-                    <h2 style='text-align: center; font-size: 40px; color: red;'>
-                        Find your parts
-                    </h2>
-                """, unsafe_allow_html=True)
-            
-            with col2:
-                search_keyword = st.session_state.get('search_keyword', '')
-                search_keyword = st.text_input("Enter keyword to search:", search_keyword)
-                search_button = st.button("Search")
-                search_option = 'All Columns'
-            
-            def search_in_dataframe(df_Material, keyword, option):
-                if option == 'All Columns':
-                    result = df_Material[df_Material.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
-                else:
-                    result = df_Material[df_Material[option].astype(str).str.contains(keyword, case=False)]
-                return result
-            
-            if st.session_state.get('refreshed', False):
-                st.session_state.search_keyword = ''
-                st.session_state.refreshed = False
-            
-            if search_button and search_keyword:
-                st.session_state.search_keyword = search_keyword
-                search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
-                st.write(f"Search results for '{search_keyword}'in{search_option}:")
-                st.dataframe(search_results, width=1000, height=200)
-            st.session_state.refreshed = True 
-            
-            tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
-                'Reel Label (Small)', 'Reel Label (Large)',
-                'Ink Reels for Label', 'Red Tape', 'Adhasive Tape', 'Cartridges', 'MultiPharma Cartridge'
-            ])
-            
-            with tab1:
-                display_tab('Reel Label (Small)', 20)
-            with tab2:
-                display_tab('Reel Label (Large)', 60)
-            with tab3:
-                display_tab('Ink Reels for Label', 20)
-            with tab4:
-                display_tab('Red Tape', 5)
-            with tab5:
-                display_tab('Adhasive Tape', 100)
-            with tab6:
-                display_tab('Cartridges', 50)
-            with tab7:
-                display_tab('MultiPharma Cartridge', 5)
+        
+        with col2:
+            search_keyword = st.session_state.get('search_keyword', '')
+            search_keyword = st.text_input("Enter keyword to search:", search_keyword)
+            search_button = st.button("Search")
+            search_option = 'All Columns'
+        
+        def search_in_dataframe(df_Material, keyword, option):
+            if option == 'All Columns':
+                result = df_Material[df_Material.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
+            else:
+                result = df_Material[df_Material[option].astype(str).str.contains(keyword, case=False)]
+            return result
+        
+        if st.session_state.get('refreshed', False):
+            st.session_state.search_keyword = ''
+            st.session_state.refreshed = False
+        
+        if search_button and search_keyword:
+            st.session_state.search_keyword = search_keyword
+            search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
+            st.write(f"Search results for '{search_keyword}'in{search_option}:")
+            st.dataframe(search_results, width=1000, height=200)
+        st.session_state.refreshed = True 
+        
+        tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
+            'Reel Label (Small)', 'Reel Label (Large)',
+            'Ink Reels for Label', 'Red Tape', 'Adhasive Tape', 'Cartridges', 'MultiPharma Cartridge'
+        ])
+        
+        with tab1:
+            display_tab('Reel Label (Small)', 20)
+        with tab2:
+            display_tab('Reel Label (Large)', 60)
+        with tab3:
+            display_tab('Ink Reels for Label', 20)
+        with tab4:
+            display_tab('Red Tape', 5)
+        with tab5:
+            display_tab('Adhasive Tape', 100)
+        with tab6:
+            display_tab('Cartridges', 50)
+        with tab7:
+            display_tab('MultiPharma Cartridge', 5)
 
-            st.button("Update page")
-            csv = st.session_state.df.to_csv(index=False)
-            st.download_button(label="Download updated sheet", data=csv, file_name='matril.csv', mime='text/csv')
-    
+        st.button("Update page")
+        csv = st.session_state.df.to_csv(index=False)
+        st.download_button(label="Download updated sheet", data=csv, file_name='matril.csv', mime='text/csv')
+
     elif page == 'View Logs':
         st.header("View Logs")
         logs_df = pd.DataFrame(st.session_state.logs)
